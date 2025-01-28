9a2551d38ff3179cdb2bccd9179755732a26e1c9
Andrey Oliveira <dedey.cardoso@gmail.com>
2025-01-27 23:44:40 -0300

    starting form

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,8 +1,23 @@
 export function Home() {
   return (
     <div className="border-2 border-red-500 p-4 rounded-md">
-      <h1>Home page</h1>
-      <p>Just some comments here!</p>
+      <form action="#" className="max-w-sm mx-auto">
+        <h2>Dados do Autor</h2>
+        <div className="mb-5">
+          <label
+            htmlFor="name"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Nome
+          </label>
+          <input
+            type="text"
+            id="name"
+            className="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            required
+          />
+        </div>
+      </form>
     </div>
   );
 }
