a3317682ed03db44ca1ed18d367a93da5599e476
= <=>
2025-05-27 10:24:53 -0300

    generate catalog card pdf

--- client/src/api/requests.ts
+++ client/src/api/requests.ts
@@ -1,6 +1,18 @@
 import { api } from "./api";
 import type { createCatalogCardData } from "../components/Home/Home"
 
-export const generateCatalogCard = (data: createCatalogCardData) => {
-  api.post("/generate_catalog_card", data);
+export const generateCatalogCard = async (data: createCatalogCardData) => {
+  try {
+    const response = await api.post("/generate_catalog_card", 
+      data, {
+      responseType: "blob"
+    });
+
+    const file = new Blob([response.data], {type: "application/pdf"});
+    const fileURL = URL.createObjectURL(file);
+
+    window.open(fileURL, "_blank");
+  } catch (error) {
+    console.error("Erro ao gerador o PDF: ", error);
+  }
 };
